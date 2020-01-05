0026eccc6d01b1f0d1af3597ad42b2354a1e2377
sAnjali12 <anjalisen060@gmail.com>
2020-01-05 16:30:31 +0530

    get data and put string length limit for description

--- route/product.js
+++ route/product.js
@@ -26,17 +26,31 @@ product.get("/products/:product_id/",(req,res)=>{
    })
 });
 
-product.get("/products/search/:limit/:product_name/",(req,res)=>{
+
+product.get("/products/search/:limit/:product_name/:length",(req,res)=>{
     var product_name = req.params.product_name
     var limit = req.params.limit
+    var description_length = req.params.length
     var data =productDb.select_productName(product_name, limit)
-   data.then((Response)=>{
-       res.send(Response)
-   }).catch((err)=>{
-       console.log(err)
+    data.then((Response)=>{
+        var description = Response[0]['description']
+        var sort_description = description.slice(0,description_length);
+        Response["description"] = sort_description
+        main_data ={
+            "product_id":Response[0]['product_id'],
+            "name":Response[0]['name'],
+            "description":sort_description,
+            "price": Response[0]['price'],
+            "discounted_price": Response[0]['discounted_price'],
+            "thumbnail": Response[0]['thumbnail']
+        }
+        res.send(main_data)
+    }).catch((err)=>{
        res.send(err)
    })
 });
+
+
 
 product.get("/products/inCategory/:category_id",(req,res)=>{
     var category_id  = req.params.category_id
@@ -56,7 +70,6 @@ product.get("/products/inDepartment/:department_id",(req,res)=>{
    data.then((Response)=>{
        res.send(Response)
    }).catch((err)=>{
-       console.log(err)
        res.send(err)
    })
 });
@@ -89,7 +102,7 @@ product.post("/products/:product_id/reviews",(req,res)=>{
         "customer_id": req.body.customer_id,
         "review": req.body.review,
         "rating": req.body.rating,
-        "created_on": req.body.created_on 
+        "created_on": req.body.created_on
     }
     reviews["product_id"] = req.params.product_id
     productDb.insertdata_review(reviews).then((data)=>{
