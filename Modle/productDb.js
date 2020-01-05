43ae84c04dfe25f8158fb76763848e1e128b5969
sAnjali12 <anjalisen060@gmail.com>
2020-01-05 12:28:12 +0530

    created two folders for Routes and Modle

---
+++ Modle/productDb.js
@@ -0,0 +1,36 @@
+var knex = require("./conection.js");
+
+var selectData = ()=>{
+    return knex.select("*").from("product")
+}
+
+var select_productId = (product_id)=>{
+    return knex.select("*").from("product").where("product_id", product_id)
+}
+
+var select_productName = (product_name,limit)=>{
+    return knex.select(" product.product_id", "product.name", "product.description", "product.price", "product.discounted_price" , "product.thumbnail").from("product").limit(limit)
+    .where("name",product_name)
+    
+}
+
+var join_categorytable = (category_id )=>{
+    return knex.select(" product.product_id", "product.name", "product.description", "product.price", "product.discounted_price" , "product.thumbnail").from("product").innerJoin('category',"product.product_id", "category.category_id").where("category_id",category_id)
+    
+}
+
+var join_departmenttable = (department_id)=>{
+    return knex.select(" product.product_id", "product.name", "product.description", "product.price", "product.discounted_price" , "product.thumbnail").from("product").innerJoin('department',"product.product_id", "department.department_id").where("department_id",department_id)
+    
+}
+
+var productDetail = (product_id)=>{
+    return knex.select("product_id", "name", "description", "price", "discounted_price", "image", "image_2 ").from("product").where("product_id", product_id)
+}
+
+var join_categorytableLocations = (product_id )=>{
+    return knex.select("category.name","department.department_id","category.category_id", "department.name").from('product').innerJoin('department').innerJoin('category').where('product_id',product_id)
+}
+
+
+module.exports = {selectData, select_productId, select_productName, join_categorytable , join_departmenttable, productDetail, join_categorytableLocations}
---
+++ route/product.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const product = express.Router();
+const productDb = require("../Modle/productDb");
+product.use(express.json())
+
+
+product.get("/products",(req,res)=>{
+    var data = productDb.selectData()
+    data.then((Response)=>{
+       res.json(Response)
+   }).catch((err)=>{
+       console.log(err)
+       res.send(err)
+   })
+});
+
+
+product.get("/products/:product_id/",(req,res)=>{
+    var product_id  = req.params.product_id
+    var data =productDb.select_productId(product_id)
+   data.then((Response)=>{
+       res.send(Response)
+   }).catch((err)=>{
+       console.log(err)
+       res.send(err)
+   })
+});
+
+product.get("/products/search/:limit/:product_name/",(req,res)=>{
+    var product_name = req.params.product_name
+    var limit = req.params.limit
+    var data =productDb.select_productName(product_name, limit)
+   data.then((Response)=>{
+       res.send(Response)
+   }).catch((err)=>{
+       console.log(err)
+       res.send(err)
+   })
+});
+
+product.get("/products/inCategory/:category_id",(req,res)=>{
+    var category_id  = req.params.category_id
+    var data =productDb.join_categorytable(category_id)
+   data.then((Response)=>{
+       res.send(Response)
+   }).catch((err)=>{
+       console.log(err)
+       res.send(err)
+   })
+});
+
+
+product.get("/products/inDepartment/:department_id",(req,res)=>{
+    var department_id  = req.params.department_id
+    var data =productDb.join_departmenttable(department_id)
+   data.then((Response)=>{
+       res.send(Response)
+   }).catch((err)=>{
+       console.log(err)
+       res.send(err)
+   })
+});
+
+product.get("/products/:product_id/details",(req,res)=>{
+    var product_id  = req.params.product_id
+    var data =productDb.productDetail(product_id)
+   data.then((Response)=>{
+       res.send(Response)
+   }).catch((err)=>{
+       console.log(err)
+       res.send(err)
+   })
+});
+
+
+product.get("/products/:product_id/locations",(req,res)=>{
+    var product_id  = req.params.product_id
+    var data =productDb.join_categorytableLocations(product_id)
+   data.then((Response)=>{
+       res.send(Response)
+   }).catch((err)=>{
+       console.log(err)
+       res.send(err)
+   })
+});
+
+product.post("/products/:product_id/reviews")
+
+
+
+
+module.exports = product;
+
+
+
+
