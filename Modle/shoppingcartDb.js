f68f1fdc9b07d49b9c04f6ab0d21860e6b71412c
sAnjali12 <anjalisen060@gmail.com>
2020-01-08 20:24:00 +0530

    select, insert and delete query for data get, post data and delete data

--- Modle/shoppingcartDb.js
+++ Modle/shoppingcartDb.js
@@ -27,6 +27,16 @@ var totalAmount = (cart_id)=>{
     return knex.select("*").from("shopping_cart").innerJoin("product", "shopping_cart.product_id", "product.product_id").where("shopping_cart.cart_id", cart_id)
 }
 
+var selectbyitem_id = (item_id)=>{
+    return knex.select("shopping_cart.cart_id", "shopping_cart.product_id", "shopping_cart. attributes", "shopping_cart.quantity", "shopping_cart.buy_now", "shopping_cart. added_on").from("shopping_cart").where("item_id",item_id)
+}
 
+var insertData_saveForLater = (saveForLater)=>{
+    return knex("saveForLater").insert(saveForLater)
+}
 
-module.exports = {selectData, insertData, selectby_id, joinProduct, deleteData,totalAmount }
+var deleteDatabyitem_id = (item_id)=>{
+    return knex.select("*").from("shopping_cart ").where("item_id", item_id).del()
+}
+
+module.exports = {selectData, insertData, selectby_id, joinProduct, deleteData,totalAmount,selectbyitem_id, insertData_saveForLater, deleteDatabyitem_id}
