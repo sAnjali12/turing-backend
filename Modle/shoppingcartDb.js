a8da22ba09fdeaf2b41945f7872e7032425fb92d
sAnjali12 <anjalisen060@gmail.com>
2020-01-08 16:31:17 +0530

    updata data with join  query product table

--- Modle/shoppingcartDb.js
+++ Modle/shoppingcartDb.js
@@ -13,4 +13,9 @@ var selectby_id = (cart_id)=>{
     return knex.select("*").from("shopping_cart ").where("cart_id",cart_id)
 }
 
-module.exports = {selectData, insertData, selectby_id}
+
+joinProduct = (item_id)=>{
+    return knex.select("shopping_cart.item_id","product.name","shopping_cart.attributes","shopping_cart.quantity","product.product_id","product.price").from("shopping_cart").innerJoin("product", "shopping_cart.product_id", "product.product_id").where("shopping_cart.item_id", item_id)
+}
+
+module.exports = {selectData, insertData, selectby_id, joinProduct}
