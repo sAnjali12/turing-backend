2c383da6fd67da9f74b400714afe0c6e6da1b4ff
sAnjali12 <anjalisen060@gmail.com>
2020-01-08 12:05:28 +0530

    updata data by customer_id

--- Modle/customerDb.js
+++ Modle/customerDb.js
@@ -17,6 +17,14 @@ var user_login = ()=>{
    return knex.select("*").from("customer")
 }
 
-module.exports = {insertData, selectData, updateData, user_login
+var putcustomerAddress = (customerDetail, customer_id)=>{
+   return knex.select("*").from("customer").where("customer_id", customer_id).update(customerDetail)
+};
+
+var putcustomerCredit_card = (updateCredit_card, customer_id)=>{
+   return knex.select("*").from("customer").where("customer_id", customer_id).update(updateCredit_card)
+}
+
+module.exports = {insertData, selectData, updateData, user_login, putcustomerAddress, putcustomerCredit_card
 
 }
