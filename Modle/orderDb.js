bcc62a1e47342bc9b7560cd58bf068a5b8c4cefd
sAnjali12 <anjalisen060@gmail.com>
2020-01-09 13:30:16 +0530

    select query with join product table

--- Modle/orderDb.js
+++ Modle/orderDb.js
@@ -5,8 +5,17 @@ var getWithJoin = (cart_id)=>{
     return knex.select("shopping_cart. quantity", "product.price").from("shopping_cart").innerJoin("product", "shopping_cart.product_id", "product.product_id").where("shopping_cart.cart_id", cart_id)
 }
 
-var insertData = (orderDetail)=>{
-    return knex("orders").insert(orderDetail)
+var insertData = (orders)=>{
+    return knex("orders").insert(orders)
 }
 
-module.exports = {getWithJoin, insertData}
+var orderDetail  = (order_id)=>{
+    return knex.select("orders.order_id","orders.total_amount", "orders.created_on", "orders.shipped_on", "orders.status", "product.name").from("orders").innerJoin("product", "orders.order_id", "product.product_id").where("orders.order_id", order_id)
+}
+
+
+module.exports = {getWithJoin, insertData,orderDetail}
+
+
+
+
